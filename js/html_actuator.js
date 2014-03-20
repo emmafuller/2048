2fd9cb030ccfa5645852957751d786933e1559a8
Emma Fuller <emma@mobisocial.us>
2014-03-20 15:44:41 -0700

    Removing Tweet, and making it a "Share" button

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -152,17 +152,14 @@ HTMLActuator.prototype.clearMessage = function () {
 };
 
 HTMLActuator.prototype.scoreTweetButton = function () {
-  var tweet = document.createElement("a");
-  tweet.classList.add("twitter-share-button");
-  tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
-  tweet.textContent = "Tweet";
+  var share = document.createElement("a");
+  share.textContent = "Share";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
-  tweet.setAttribute("data-text", text);
+  var text = "I scored " + this.score + " points at 2048.";
+			 
+             share.addListener("click",function(event){
+               alert(text);
+             });
 
-  return tweet;
+  return share;
 };
